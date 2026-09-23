cf72ca4c8eecd2b87ac6de781e79ee5f38b66ed3
Serpenseth <enkisaur@tutanota.com>
2026-09-23 21:21:40 +0300

    move DomQuery to utilities.js
    - split hideCard into separate function

--- renderer/screens/OllamaInstructions.js
+++ renderer/screens/OllamaInstructions.js
@@ -1,6 +1,8 @@
 import { OllamaDetected } from './OllamaDetected.js';
 import { OllamaSuccess } from './OllamaSuccess.js';
 
+import { DomQuery } from '../utility/utilities.js';
+
 const INSTALL_CONFIGS = {
   macOS: {
     shell: 'Terminal',
@@ -83,60 +85,6 @@ const OllamaInstructionsModal = {
   }
 };
 
-const DomQuery = {
-  getElement: (id) => document.getElementById(id),
-
-  removeElement(id) {
-    const el = this.getElement(id);
-    if (el)
-      el.remove();
-  },
-
-  insertHTML(containerId, html) {
-    const container = this.getElement(containerId);
-
-    if (container)
-      container.insertAdjacentHTML('beforeend', html);
-  },
-
-  updateText(id, text) {
-    const el = this.getElement(id);
-    if (el)
-      el.textContent = text;
-  },
-
-  updateInputValue(id, value) {
-    const el = this.getElement(id);
-    if (el)
-      el.value = value;
-  },
-
-  toggleVisibility(id, isVisible) {
-    const el = this.getElement(id);
-
-    el.style.contentVisibility = isVisible ? '' : 'hidden';
-    el.style.opacity = isVisible ? 1 : 0;
-    el.style.visibility = isVisible ? 'visible' : 'hidden';
-  },
-
-  setElementClass(id, className, add = true) {
-    const el = this.getElement(id);
-
-    if (el)
-      el.classList[add ? 'add' : 'remove'](className);
-  },
-
-  toggleButton(id, isVisible) {
-    const el = this.getElement(id);
-    el.style.display = isVisible ? '' : 'none';
-  },
-
-  hideCard(id) {
-    const card = this.getElement(id);
-    card.style.display = 'none';
-  }
-};
-
 const WindowApi = {
   async checkConnection() {
     return window.electronAPI.checkOllama();
@@ -195,6 +143,11 @@ const UiHandler = {
   }
 }
 
+function hideCard(id) {
+  const card = DomQuery.getElement(id);
+  card.style.display = 'none';
+}
+
 const PlatformManager = {
   handlePlatformSelect(platform) {
     platform === 'linux'
@@ -216,7 +169,7 @@ const PlatformManager = {
   hideUnsupportedPlatforms(currentPlatform, allPlatforms) {
     Object.values(allPlatforms).forEach(card => {
       if (!card.id.includes(currentPlatform)) {
-        DomQuery.hideCard(card.id);
+        hideCard(card.id);
       }
     });
   }
