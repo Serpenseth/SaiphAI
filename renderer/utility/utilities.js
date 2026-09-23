20664112b6e99aa975a4ab2ad4090410df560ed3
Serpenseth <enkisaur@tutanota.com>
2026-09-23 20:40:16 +0300

    move DomQuery to utilities.js

--- renderer/utility/utilities.js
+++ renderer/utility/utilities.js
@@ -4,3 +4,58 @@ export const StringUtils = {
     return !element ? id.substring(id?.id.lastIndexOf('-') + 1) : element;
   },
 }
+
+export const DomQuery = {
+  getElement: (id) => document.getElementById(id),
+
+  removeElement(id) {
+    const el = this.getElement(id);
+    if (el)
+      el.remove();
+  },
+
+  insertHTML(containerId, html) {
+    const container = this.getElement(containerId);
+
+    if (container)
+      container.insertAdjacentHTML('beforeend', html);
+  },
+
+  updateText(id, text) {
+    const el = this.getElement(id);
+    if (el)
+      el.textContent = text;
+  },
+
+  updateInputValue(id, value) {
+    const el = this.getElement(id);
+    if (el)
+      el.value = value;
+  },
+
+  toggleVisibility(id, isVisible) {
+    const el = this.getElement(id);
+
+    el.style.contentVisibility = isVisible ? '' : 'hidden';
+    el.style.opacity = isVisible ? 1 : 0;
+    el.style.visibility = isVisible ? 'visible' : 'hidden';
+  },
+
+  setElementClass(id, className, add = true) {
+    const el = this.getElement(id);
+
+    if (el)
+      el.classList[add ? 'add' : 'remove'](className);
+  },
+
+  toggleButton(id, isVisible) {
+    const el = this.getElement(id);
+    el.style.display = isVisible ? '' : 'none';
+  },
+
+  hideCard(id) {
+    const card = this.getElement(id);
+    card.style.display = 'none';
+  }
+};
+
